bfe6a60620e14dea3deef1a88dfff2a9de53318d
undefinedot <1055821119@qq.com>
2025-05-09 19:57:59 +0800

    feat: use localStorage and simple duplicate task name

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,8 @@ const activeEventIndex = ref(0)
 
 // Event: {title: '事件标题', todo: [{'taskName': 'decription'}], doing:[], completed: []}
 
-const eventList = ref(tasksData)
+localStorage.setItem('AllEvents', JSON.stringify(tasksData))
+const eventList = ref(JSON.parse(localStorage.getItem('AllEvents')))
 
 const addTheEvent = (t) => {
   eventList.value.push({
@@ -25,25 +26,41 @@ const addTheEvent = (t) => {
     doing: [],
     completed: []
   })
+  localStorage.setItem('AllEvents', JSON.stringify(eventList.value))
+
 }
 
 const removeTheEvent = () => {
   if (!confirm(`确定删除事件【${eventList.value[activeEventIndex.value].title}】吗？`)) return
-  eventList.value = eventList.value.filter((item, index) => {
+
+  let eventListArray = []
+  eventListArray = eventList.value.filter((item, index) => {
     return index != activeEventIndex.value
   })
-  if (eventList.value.length < 1) {
-    eventList.value = [{
+  if (eventListArray.length < 1) {
+    eventListArray = [{
       title: '创建一个事件吧 ^^',
       todo: [],
       doing: [],
       completed: []
     }]
   }
+
+  localStorage.setItem('AllEvents', JSON.stringify(eventListArray))
+  eventList.value = JSON.parse(localStorage.getItem('AllEvents'))
 }
 
 const addTask = (obj, key) => {
-  eventList.value[activeEventIndex.value][key].push(obj)
+  // 简单去重
+  if (eventList.value[activeEventIndex.value][key].some((item) => {
+    return item.name.toLowerCase() === obj.name.toLowerCase()
+  })) {
+    alert('事件名已存在，请重新添加')
+    return
+  } else {
+    eventList.value[activeEventIndex.value][key].push(obj)
+    localStorage.setItem('AllEvents', JSON.stringify(eventList.value))
+  }
 }
 </script>
 
