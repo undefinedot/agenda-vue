96bf018fb09d10b99cc3f909c2bf4a16d180c885
undefinedot <1055821119@qq.com>
2025-05-09 17:26:21 +0800

    feat: TaskList css

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -5,7 +5,26 @@
             <button class="btn-remove" @click="emit('btn-remove')">删除事件</button>
         </nav>
         <div class="task-list">
-
+            <div class="task-column">
+                <h3>待办</h3>
+                <button class="btn-add-task">+</button>
+                <ul>
+                    <li class="task-item">
+                        <div class="title">标题</div>
+                        <p>description</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="task-column">
+                <h3>进行中</h3>
+                <button class="btn-add-task">+</button>
+                <ul></ul>
+            </div>
+            <div class="task-column">
+                <h3>已完成</h3>
+                <button class="btn-add-task">+</button>
+                <ul></ul>
+            </div>
         </div>
     </div>
 </template>
@@ -17,13 +36,14 @@ const emit = defineEmits(['btn-remove'])
 <style scoped>
 .task-bar {
     flex: 3;
-    margin: 3rem 2rem;
+    padding: 2rem 0.8rem;
 }
 
 nav {
     display: flex;
     align-items: center;
     gap: 2rem;
+    margin-bottom: 2rem;
 }
 
 .capital {
@@ -45,5 +65,79 @@ nav {
 .btn-remove:hover {
     opacity: 0.8;
     cursor: pointer;
+}
+
+.task-list {
+    height: 700px;
+    display: flex;
+    gap: 1rem;
+    align-items: flex-start;
+}
+
+.task-column {
+    border-radius: 10px;
+    background: rgb(219, 214, 214);
+    flex: 1;
+    min-width: 100px;
+    max-height: 700px;
+    padding: 1rem 1rem;
+}
+
+.btn-add-task {
+    display: block;
+    width: 80%;
+    height: 40px;
+    background: rgb(205, 193, 193);
+    border: none;
+    border-radius: 10px;
+    margin: 1rem auto;
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    padding-bottom: 3px;
+}
+
+.btn-add-task:hover {
+    cursor: pointer;
+    color: white;
+    background: rgb(205, 193, 193, 0.8);
+    transition: 0.3s;
+}
+
+.task-column ul {
+    padding-top: 1rem;
+    min-height: 30px;
+    max-height: 600px;
+    overflow: auto;
+}
+
+.task-item {
+    width: 80%;
+    min-height: 4rem;
+    border-radius: 10px;
+    background: #f0f0f0;
+    margin: 0 auto 0.8rem;
+    padding: 1rem 0.6rem;
+}
+
+.task-item .title {
+    font-size: 1.3rem;
+    line-height: 1.3rem;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    font-style: italic;
+}
+
+.task-item .title::before {
+    content: '\25cf';
+    color: #fb7299;
+    margin-right: 0.5rem;
+    font-style: normal;
+}
+
+.task-item p {
+    color: rgba(241, 163, 18, 0.8);
+    overflow-wrap: break-word;
+    margin: 8px 10px 0;
 }
 </style>
